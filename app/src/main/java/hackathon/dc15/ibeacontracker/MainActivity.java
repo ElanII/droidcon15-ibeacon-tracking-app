7bfde4f44a4fbd6cdc04aa085fb9baeedb09a333
nohum <eiskalterengel@truthfactory.tk>
2015-06-06 13:15:23 +0200

    added websocket

--- app/src/main/java/hackathon/dc15/ibeacontracker/MainActivity.java
+++ app/src/main/java/hackathon/dc15/ibeacontracker/MainActivity.java
@@ -1,17 +1,10 @@
 package hackathon.dc15.ibeacontracker;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.RemoteException;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.WindowManager;
-import android.widget.EditText;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -19,11 +12,16 @@ import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
+
+import de.roderick.weberknecht.WebSocket;
 
 public class MainActivity extends ActionBarActivity implements BeaconConsumer {
 
     private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+    private WebSocket websocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +30,45 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
         setContentView(R.layout.activity_main);
 
         beaconManager.bind(this);
+
+        try {
+            URI url = new URI("ws://100.100.238.92:3000");
+//            websocket = new WebSocket(url);
+//
+//            websocket.setEventHandler(new WebSocketEventHandler() {
+//                public void onOpen() {
+//                    Log.i("MainActivity", "websocket is open");
+//                }
+//
+//                public void onMessage(WebSocketMessage message)  {
+//                    Log.i("MainActivity", "websocket received message: " + message.toString());
+//                }
+//
+//                @Override
+//                public void onError(IOException exception) {
+//                    Log.e("MainActivity", "websocket error happened", exception);
+//                }
+//
+//                public void onClose() {
+//                    Log.i("MainActivity", "websocket has been closed");
+//                }
+//
+//                public void onPing() {}
+//                public void onPong() {}
+//            });
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         beaconManager.unbind(this);
+
+        if (websocket != null) {
+            websocket.close();
+        }
     }
 
     @Override
@@ -61,15 +92,10 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
         beaconManager.setRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                if (beacons.size() > 0) {
-
-                    for (Beacon beacon : beacons) {
-                        // TODO make configurable
-                        if (beacon.getId1().toString().contains("f0018b9b-7509-4c31-a905-1a27d39c003c")) {
-                            Log.i("MainActivity", "Found beacon -> " + beacon.toString() + " is about " + beacon.getDistance() + " meters away.");
-                            Log.i("MainActivity", "-> Strength " + beacon.getRssi());
-                            Log.i("MainActivity", "-> TxPower " + beacon.getTxPower());
-                        }
+                for (Beacon beacon : beacons) {
+                    // TODO make configurable
+                    if (beacon.getId1().toString().contains("f0018b9b-7509-4c31-a905-1a27d39c003c")) {
+                        handleBeacon(beacon);
                     }
                 }
             }
@@ -81,5 +107,13 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
             e.printStackTrace();
         }
 
+    }
+
+    private void handleBeacon(Beacon beacon) {
+        Log.i("MainActivity", "Found beacon -> " + beacon.toString() + " is about " + beacon.getDistance() + " meters away.");
+        Log.i("MainActivity", "-> Strength " + beacon.getRssi());
+        Log.i("MainActivity", "-> TxPower " + beacon.getTxPower());
+
+//        websocket.send("hello world");
     }
 }
