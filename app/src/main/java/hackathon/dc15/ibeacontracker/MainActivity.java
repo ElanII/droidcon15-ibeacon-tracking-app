c50e563fbdc1dd5ce42685777811e84d8d91d72f
nohum <eiskalterengel@truthfactory.tk>
2015-06-06 15:13:02 +0200

    device logging

--- app/src/main/java/hackathon/dc15/ibeacontracker/MainActivity.java
+++ app/src/main/java/hackathon/dc15/ibeacontracker/MainActivity.java
@@ -6,6 +6,7 @@ import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -14,6 +15,8 @@ import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 
 public class MainActivity extends ActionBarActivity implements BeaconConsumer {
@@ -25,20 +28,27 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
 
     private String android_id;
 
+    private TextView textLastSend;
+    private TextView textStrength;
+    private DateFormat formatter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
 
+        textLastSend = (TextView) findViewById(R.id.textLastSend);
+        TextView textDeviceId = (TextView) findViewById(R.id.textDeviceId);
+        textStrength = (TextView) findViewById(R.id.textStrength);
+
         android_id = Settings.Secure.getString(this.getContentResolver(),
                 Settings.Secure.ANDROID_ID);
+        textDeviceId.setText("Device id: " + android_id);
 
+        formatter = new SimpleDateFormat("yyyy-mm-dd HH:MM:ss");
 
         beaconManager.bind(this);
-
-        sender = new WebSocketSender("http://100.100.238.92:3000");
-        sender.connect();
     }
 
     @Override
@@ -54,6 +64,8 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
         if (beaconManager.isBound(this)) {
             beaconManager.setBackgroundMode(true);
         }
+
+        sender.disconnect();
     }
 
     @Override
@@ -62,6 +74,9 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
         if (beaconManager.isBound(this))  {
             beaconManager.setBackgroundMode(false);
         }
+
+        sender = new WebSocketSender("http://100.100.238.92:3000");
+        sender.connect();
     }
 
     @Override
@@ -85,7 +100,7 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
         }
     }
 
-    private void handleBeacon(Beacon beacon) {
+    private void handleBeacon(final Beacon beacon) {
         if (System.currentTimeMillis() < nextSendTime) {
             Log.i("MainActivity", "-> DISCARDING");
             return;
@@ -94,6 +109,14 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
 
         Log.i("MainActivity", "Found beacon -> " + beacon.toString() + " is about " + beacon.getDistance() + " meters away.");
         Log.i("MainActivity", "-> Strength " + beacon.getRssi());
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                textLastSend.setText("Last report: " + formatter.format(System.currentTimeMillis()));
+                textStrength.setText("Strength: " + beacon.getRssi());
+            }
+        });
 
         try {
             JSONObject data = new JSONObject();
@@ -105,9 +128,7 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
 
             sender.send(data);
         } catch (Exception e) {
-
             Log.i("MainActivity", "failed creating json", e);
         }
-
     }
 }
